9c4305fca6316e137ccedc2a1be306dcaf01cc63
Parabeac-Nest <admin@parabeac.com>
2022-10-26 11:45:43 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/kadeca_colors.g.dart
@@ -0,0 +1,14 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class KadecaColors {
+
+static const Color primary = Color(0xffeef07b);
+
+static const Color secondary = Color(0xff164fe2);
+}
---
+++ lib/theme/kadeca_text_styles.g.dart
@@ -0,0 +1,20 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class KadecaTextStyles {
+
+static const TextStyle header2 = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.w500,
+  letterSpacing: 0,
+  fontFamily: 'Inter',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+}
