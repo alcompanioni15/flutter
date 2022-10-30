1dad9a0fcb0b8248b8d7354f0fa621d87727ebbc
Parabeac-Nest <admin@parabeac.com>
2022-10-30 02:03:19 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/widgets/home_page/menu_item/property1_active.g.dart
@@ -0,0 +1,83 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+//
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+
+import 'package:flutter/material.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class Property1Active extends StatefulWidget {
+  final BoxConstraints constraints;
+  final String? ovrHome;
+  final Widget? ovrLine1;
+  const Property1Active(
+    this.constraints, {
+    Key? key,
+    this.ovrHome,
+    this.ovrLine1,
+  }) : super(key: key);
+  @override
+  _Property1Active createState() => _Property1Active();
+}
+
+class _Property1Active extends State<Property1Active> {
+  _Property1Active();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(),
+        child: Stack(children: [
+          Positioned(
+            left: 0,
+            width: widget.constraints.maxWidth * 1.0,
+            top: 0,
+            height: widget.constraints.maxHeight * 1.0,
+            child: Stack(children: [
+              Positioned(
+                left: widget.constraints.maxWidth * 0.094,
+                width: widget.constraints.maxWidth * 0.811,
+                top: 0,
+                height: widget.constraints.maxHeight * 1.0,
+                child: Center(
+                    child: Container(
+                        height: 18.0,
+                        width: 43.0,
+                        child: AutoSizeText(
+                          widget.ovrHome ?? 'Home',
+                          style: TextStyle(
+                            fontFamily: 'Roboto',
+                            fontSize: 15,
+                            fontWeight: FontWeight.w600,
+                            letterSpacing: 0.75,
+                            color: Colors.black,
+                          ),
+                          textAlign: TextAlign.center,
+                        ))),
+              ),
+              Positioned(
+                left: 5.0,
+                width: 43.0,
+                top: 17.0,
+                height: 1.0,
+                child: widget.ovrLine1 ??
+                    SvgPicture.asset(
+                      'assets/images/line1.svg',
+                      package: 'kadeca',
+                      height: 1.0,
+                      width: 43.0,
+                      fit: BoxFit.fitHeight,
+                    ),
+              ),
+            ]),
+          ),
+        ]));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
---
+++ lib/widgets/home_page/menu_item/property1_normal.g.dart
@@ -0,0 +1,58 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+//
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+
+import 'package:flutter/material.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+
+class Property1Normal extends StatefulWidget {
+  final BoxConstraints constraints;
+  final String? ovrHome;
+  const Property1Normal(
+    this.constraints, {
+    Key? key,
+    this.ovrHome,
+  }) : super(key: key);
+  @override
+  _Property1Normal createState() => _Property1Normal();
+}
+
+class _Property1Normal extends State<Property1Normal> {
+  _Property1Normal();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(),
+        child: Stack(children: [
+          Positioned(
+            left: widget.constraints.maxWidth * 0.094,
+            width: widget.constraints.maxWidth * 0.811,
+            top: 0,
+            height: widget.constraints.maxHeight * 1.0,
+            child: Center(
+                child: Container(
+                    height: 18.0,
+                    width: 43.0,
+                    child: AutoSizeText(
+                      widget.ovrHome ?? 'Home',
+                      style: TextStyle(
+                        fontFamily: 'Roboto',
+                        fontSize: 15,
+                        fontWeight: FontWeight.w600,
+                        letterSpacing: 0.75,
+                        color: Colors.black,
+                      ),
+                      textAlign: TextAlign.center,
+                    ))),
+          ),
+        ]));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
