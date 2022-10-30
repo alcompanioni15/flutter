924b8a5604c326ee6c63a00bb35c14adc3c4c59c
Parabeac-Nest <admin@parabeac.com>
2022-10-30 01:57:47 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/views/home_page/buttons.g.dart
@@ -0,0 +1,31 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+//
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+
+import 'package:flutter/material.dart';
+
+class Buttons extends StatefulWidget {
+  const Buttons({
+    Key? key,
+  }) : super(key: key);
+  @override
+  _Buttons createState() => _Buttons();
+}
+
+class _Buttons extends State<Buttons> {
+  _Buttons();
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white,
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
