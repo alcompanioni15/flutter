964a991e28ce6d944138b8c0396fad5683f9921e
Parabeac-Nest <admin@parabeac.com>
2022-10-30 01:08:43 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/views/home_page/components.g.dart
@@ -0,0 +1,31 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+//
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+
+import 'package:flutter/material.dart';
+
+class Components extends StatefulWidget {
+  const Components({
+    Key? key,
+  }) : super(key: key);
+  @override
+  _Components createState() => _Components();
+}
+
+class _Components extends State<Components> {
+  _Components();
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white,
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
