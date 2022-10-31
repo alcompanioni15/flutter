b166aab4cf9e5e136c1d4f103db1ff5074913c35
Parabeac-Nest <admin@parabeac.com>
2022-10-31 00:51:16 +0000

    Auto-generated commit by Parabeac Cloud

--- lib/widgets/home_page/navbar.g.dart
+++ lib/widgets/home_page/navbar.g.dart
@@ -5,20 +5,24 @@
 // *********************************************************************************
 
 import 'package:flutter/material.dart';
-import 'package:auto_size_text/auto_size_text.dart';
-import 'package:kadeca/widgets/home_page/custom/menu_navbar_custom.dart';
-import 'package:kadeca/widgets/home_page/custom/menu_item_custom.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:kadeca/widgets/home_page/menu_item/state_active.g.dart';
 import 'package:kadeca/widgets/home_page/menu_item/state_normal.g.dart';
 
 class Navbar extends StatefulWidget {
   final BoxConstraints constraints;
   final String? ovrKadeca;
+  final Widget? ovrHome;
+  final Widget? ovrContact;
+  final Widget? ovrAbout;
   const Navbar(
     this.constraints, {
     Key? key,
     this.ovrKadeca,
+    this.ovrHome,
+    this.ovrContact,
+    this.ovrAbout,
   }) : super(key: key);
   @override
   _Navbar createState() => _Navbar();
@@ -35,94 +39,78 @@ class _Navbar extends State<Navbar> {
         ),
         child: Stack(children: [
           Positioned(
-            left: 0,
-            width: widget.constraints.maxWidth * 1.0,
-            top: 0,
-            height: widget.constraints.maxHeight * 1.0,
-            child: Stack(children: [
-              Positioned(
-                left: 17.0,
-                width: 67.0,
-                top: widget.constraints.maxHeight * 0.286,
-                height: widget.constraints.maxHeight * 0.411,
-                child: Center(
-                    child: Container(
-                        height: 23.0,
+            right: 27.0,
+            width: 428.0,
+            top: widget.constraints.maxHeight * 0.125,
+            height: widget.constraints.maxHeight * 0.75,
+            child: Center(
+                child: Container(
+                    decoration: BoxDecoration(),
+                    child: Stack(children: [
+                      Positioned(
+                        left: 1248.0,
+                        right: 29.0,
+                        top: 12.0,
+                        height: 18.0,
+                        child: LayoutBuilder(builder: (context, constraints) {
+                          return StateNormal(
+                            constraints,
+                            ovrHome: 'About',
+                          );
+                        }),
+                      ),
+                      Positioned(
+                        left: 1103.0,
+                        right: 174.0,
+                        top: 12.0,
+                        height: 18.0,
+                        child: LayoutBuilder(builder: (context, constraints) {
+                          return StateNormal(
+                            constraints,
+                            ovrHome: 'Contact',
+                          );
+                        }),
+                      ),
+                      Positioned(
+                        left: 958.0,
+                        width: 80.0,
+                        top: 12.0,
+                        height: 18.0,
+                        child: LayoutBuilder(builder: (context, constraints) {
+                          return StateActive(
+                            constraints,
+                            ovrHome: 'Home',
+                            ovrLine1: SvgPicture.asset(
+                              'assets/images/line1.svg',
+                              package: 'kadeca',
+                              height: 1.0,
+                              width: 43.0,
+                              fit: BoxFit.none,
+                            ),
+                          );
+                        }),
+                      ),
+                      Positioned(
+                        left: 0,
                         width: 67.0,
-                        child: AutoSizeText(
-                          widget.ovrKadeca ?? 'Kadeca',
-                          style: TextStyle(
-                            fontFamily: 'Roboto',
-                            fontSize: 20,
-                            fontWeight: FontWeight.w600,
-                            letterSpacing: 0,
-                            color: Colors.black,
-                          ),
-                          textAlign: TextAlign.left,
-                        ))),
-              ),
-              Positioned(
-                right: 27.0,
-                width: 428.0,
-                top: widget.constraints.maxHeight * 0.125,
-                height: widget.constraints.maxHeight * 0.75,
-                child: Center(
-                    child: Container(
-                        height: 42.0,
-                        width: 428.0,
-                        child: MenuNavbarCustom(
-                            child: Container(
-                                decoration: BoxDecoration(),
-                                child: Stack(children: [
-                                  Positioned(
-                                    left: 29.0,
-                                    width: 80.0,
-                                    top: 12.0,
-                                    height: 18.0,
-                                    child: MenuItemCustom(child: LayoutBuilder(
-                                        builder: (context, constraints) {
-                                      return StateActive(
-                                        constraints,
-                                        ovrHome: 'Home',
-                                        ovrLine1: SvgPicture.asset(
-                                          'assets/images/line1.svg',
-                                          package: 'kadeca',
-                                          height: 1.0,
-                                          width: 43.0,
-                                          fit: BoxFit.none,
-                                        ),
-                                      );
-                                    })),
-                                  ),
-                                  Positioned(
-                                    left: 174.0,
-                                    right: 174.0,
-                                    top: 12.0,
-                                    height: 18.0,
-                                    child: MenuItemCustom(child: LayoutBuilder(
-                                        builder: (context, constraints) {
-                                      return StateNormal(
-                                        constraints,
-                                        ovrHome: 'Contact',
-                                      );
-                                    })),
-                                  ),
-                                  Positioned(
-                                    left: 319.0,
-                                    right: 29.0,
-                                    top: 12.0,
-                                    height: 18.0,
-                                    child: MenuItemCustom(child: LayoutBuilder(
-                                        builder: (context, constraints) {
-                                      return StateNormal(
-                                        constraints,
-                                        ovrHome: 'About',
-                                      );
-                                    })),
-                                  ),
-                                ]))))),
-              ),
-            ]),
+                        top: 9.0,
+                        height: 23.0,
+                        child: Container(
+                            height: 23.0,
+                            width: 67.0,
+                            child: AutoSizeText(
+                              widget.ovrKadeca ?? 'Kadeca',
+                              style: TextStyle(
+                                fontFamily: 'Roboto',
+                                fontSize: 20,
+                                fontWeight: FontWeight.w600,
+                                letterSpacing: 0,
+                                color: Colors.black,
+                              ),
+                              textAlign: TextAlign.left,
+                            )),
+                      ),
+                    ]))),
           ),
         ]));
   }
