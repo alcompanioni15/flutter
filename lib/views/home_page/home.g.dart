0d9308057a9269b667f1fd92a0738ee9b7526070
Parabeac-Nest <admin@parabeac.com>
2022-11-08 01:21:31 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/views/home_page/home.g.dart
@@ -0,0 +1,42 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+//
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({
+    Key? key,
+  }) : super(key: key);
+  @override
+  _Home createState() => _Home();
+}
+
+class _Home extends State<Home> {
+  _Home();
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Color(0xffe6e6e6),
+      child: Stack(children: [
+        Positioned(
+          left: 0,
+          width: 1440.0,
+          top: 0,
+          height: 57.0,
+          child: LayoutBuilder(builder: (context, constraints) {
+            return Container(/** This Symbol was not found **/);
+          }),
+        ),
+      ]),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
