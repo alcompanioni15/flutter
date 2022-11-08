680bd008a51907a6e482f5494b29105b71faddaf
Parabeac-Nest <admin@parabeac.com>
2022-11-08 01:46:26 +0000

    Auto-generated commit by Parabeac Cloud

--- lib/widgets/home_page/topbar.g.dart
+++ lib/widgets/home_page/topbar.g.dart
@@ -10,10 +10,16 @@ import 'package:auto_size_text/auto_size_text.dart';
 class Topbar extends StatefulWidget {
   final BoxConstraints constraints;
   final String? ovrKadeca;
+  final String? ovrLogin;
+  final String? ovrAbout;
+  final String? ovrHome;
   const Topbar(
     this.constraints, {
     Key? key,
     this.ovrKadeca,
+    this.ovrLogin,
+    this.ovrAbout,
+    this.ovrHome,
   }) : super(key: key);
   @override
   _Topbar createState() => _Topbar();
@@ -30,38 +36,114 @@ class _Topbar extends State<Topbar> {
         ),
         child: Stack(children: [
           Positioned(
-            left: 27.0,
-            width: 91.0,
-            top: 8.0,
-            height: 44.0,
-            child: Container(
-                padding: EdgeInsets.only(
-                  left: 10,
-                  right: 10,
-                  top: 10,
-                  bottom: 10,
-                ),
+            left: 0,
+            width: widget.constraints.maxWidth * 1.0,
+            top: 0,
+            height: widget.constraints.maxHeight * 1.0,
+            child: Stack(children: [
+              Positioned(
+                left: 30.0,
                 width: 91.0,
-                decoration: BoxDecoration(),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Container(
-                          height: 24.0,
-                          width: 71.0,
-                          child: AutoSizeText(
-                            widget.ovrKadeca ?? 'Kadeca',
-                            style: TextStyle(
-                              fontFamily: 'Inter',
-                              fontSize: 20,
-                              fontWeight: FontWeight.w400,
-                              letterSpacing: 0,
-                              color: Colors.black,
-                            ),
-                            textAlign: TextAlign.left,
-                          )),
-                    ])),
+                top: 8.0,
+                height: 44.0,
+                child: Container(
+                    padding: EdgeInsets.only(
+                      left: 10,
+                      right: 10,
+                      top: 10,
+                      bottom: 10,
+                    ),
+                    width: 91.0,
+                    decoration: BoxDecoration(),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Container(
+                              height: 24.0,
+                              width: 71.0,
+                              child: AutoSizeText(
+                                widget.ovrKadeca ?? 'Kadeca',
+                                style: TextStyle(
+                                  fontFamily: 'Inter',
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w400,
+                                  letterSpacing: 0,
+                                  color: Colors.black,
+                                ),
+                                textAlign: TextAlign.left,
+                              )),
+                        ])),
+              ),
+              Positioned(
+                left: 1111.0,
+                width: 299.0,
+                top: 18.0,
+                height: 24.0,
+                child: Container(
+                    padding: EdgeInsets.only(
+                      left: 16,
+                      right: 16,
+                      top: 0,
+                      bottom: 0,
+                    ),
+                    decoration: BoxDecoration(),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Container(
+                              height: 24.0,
+                              width: 56.0,
+                              child: AutoSizeText(
+                                widget.ovrHome ?? 'Home',
+                                style: TextStyle(
+                                  fontFamily: 'Inter',
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w400,
+                                  letterSpacing: 0,
+                                  color: Colors.black,
+                                ),
+                                textAlign: TextAlign.left,
+                              )),
+                          SizedBox(
+                            width: 51,
+                          ),
+                          Container(
+                              height: 24.0,
+                              width: 57.0,
+                              child: AutoSizeText(
+                                widget.ovrAbout ?? 'About',
+                                style: TextStyle(
+                                  fontFamily: 'Inter',
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w400,
+                                  letterSpacing: 0,
+                                  color: Colors.black,
+                                ),
+                                textAlign: TextAlign.left,
+                              )),
+                          SizedBox(
+                            width: 51,
+                          ),
+                          Container(
+                              height: 24.0,
+                              width: 52.0,
+                              child: AutoSizeText(
+                                widget.ovrLogin ?? 'Login',
+                                style: TextStyle(
+                                  fontFamily: 'Inter',
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w400,
+                                  letterSpacing: 0,
+                                  color: Colors.black,
+                                ),
+                                textAlign: TextAlign.left,
+                              )),
+                        ])),
+              ),
+            ]),
           ),
         ]));
   }
