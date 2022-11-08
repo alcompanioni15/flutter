25809aba0bc7707cc4f9af44bd7a833b21c7a676
Parabeac-Nest <admin@parabeac.com>
2022-11-08 01:41:03 +0000

    Auto-generated commit by Parabeac Cloud

--- lib/widgets/home_page/topbar.g.dart
+++ lib/widgets/home_page/topbar.g.dart
@@ -31,23 +31,37 @@ class _Topbar extends State<Topbar> {
         child: Stack(children: [
           Positioned(
             left: 27.0,
-            width: 71.0,
-            top: 18.0,
-            height: 24.0,
+            width: 91.0,
+            top: 8.0,
+            height: 44.0,
             child: Container(
-                height: 24.0,
-                width: 71.0,
-                child: AutoSizeText(
-                  widget.ovrKadeca ?? 'Kadeca',
-                  style: TextStyle(
-                    fontFamily: 'Inter',
-                    fontSize: 20,
-                    fontWeight: FontWeight.w400,
-                    letterSpacing: 0,
-                    color: Colors.black,
-                  ),
-                  textAlign: TextAlign.left,
-                )),
+                padding: EdgeInsets.only(
+                  left: 10,
+                  right: 10,
+                  top: 10,
+                  bottom: 10,
+                ),
+                width: 91.0,
+                decoration: BoxDecoration(),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                          height: 24.0,
+                          width: 71.0,
+                          child: AutoSizeText(
+                            widget.ovrKadeca ?? 'Kadeca',
+                            style: TextStyle(
+                              fontFamily: 'Inter',
+                              fontSize: 20,
+                              fontWeight: FontWeight.w400,
+                              letterSpacing: 0,
+                              color: Colors.black,
+                            ),
+                            textAlign: TextAlign.left,
+                          )),
+                    ])),
           ),
         ]));
   }
