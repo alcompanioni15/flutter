6a0d52775039cb7b8e394737a10f843a2dcab4fc
Parabeac-Nest <admin@parabeac.com>
2022-11-08 12:32:42 +0000

    Auto-generated commit by Parabeac Cloud

--- lib/widgets/home_page/menuitems/state_inactive.g.dart
+++ lib/widgets/home_page/menuitems/state_inactive.g.dart
@@ -5,6 +5,7 @@
 // *********************************************************************************
 
 import 'package:flutter/material.dart';
+import 'package:kadeca_poc/widgets/home_page/custom/state_inactive_custom.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 
 class StateInactive extends StatefulWidget {
@@ -26,7 +27,8 @@ class _StateInactive extends State<StateInactive> {
   Widget build(BuildContext context) {
     return Container(
         decoration: BoxDecoration(),
-        child: Stack(children: [
+        child: StateInactiveCustom(
+            child: Stack(children: [
           Positioned(
             left: 0,
             width: widget.constraints.maxWidth * 1.0,
@@ -47,7 +49,7 @@ class _StateInactive extends State<StateInactive> {
                   textAlign: TextAlign.left,
                 )),
           ),
-        ]));
+        ])));
   }
 
   @override
